fcebb6645f0accb0808db8f34f2bdd5fa2b1ab52
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-03-01 10:03:49 +0800

    Added jump to controller

--- Assets/Assets/Scripts/PetController.cs
+++ Assets/Assets/Scripts/PetController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PetController : MonoBehaviour {
 	public float rotateSpeed = 150.0f;
 	public float verticalSpeed = 3.0f;
+	public int jumpHeight = 5;
+	private Rigidbody rb;
+
+	bool isGrounded() {
+		if (rb.velocity.y == 0) {
+			return true;
+		}
+
+		return false;
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -21,4 +31,10 @@ public class PetController : MonoBehaviour {
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 	}
+
+	void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) {
+            rb.velocity = new Vector3(0, jumpHeight, 0);
+        }
+    }
 }
