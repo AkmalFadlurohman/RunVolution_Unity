f160d2e4ff000a939f26017bf8f89044b02f6d69
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-03-02 21:31:20 +0800

    Added sun and night light animation

---
+++ Assets/Assets/Scripts/LampRadiates.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampRadiates : MonoBehaviour {
+	public float duration = 3.0F;
+    public float originalRange;
+    public float originalIntensity;
+    public Light lt;
+    public GameObject orbit;
+
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light>();
+        originalRange = lt.range;
+        originalIntensity = lt.intensity;
+		
+		if (orbit.transform.eulerAngles.z > 180 && orbit.transform.eulerAngles.z < 360) {
+			lt.intensity = originalIntensity;
+		} else {
+			lt.intensity = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (orbit.transform.eulerAngles.z > 180 && orbit.transform.eulerAngles.z < 360) {
+			lt.intensity = originalIntensity;
+		} else {
+			lt.intensity = 0;
+		}
+		float amplitude = Mathf.PingPong(Time.time, duration);
+        amplitude = amplitude / duration * 0.5F + 0.5F;
+        lt.range = originalRange * amplitude;
+    }
+}
---
+++ Assets/Assets/Scripts/SunOrbit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunOrbit : MonoBehaviour {
+	public int orbitSpeed = 5;
+	private Transform self;
+	private Light sun;
+	private float sunIntensity;
+
+	public float minDimDeg = 160.0f;
+	public float maxDimDeg = 180.0f;
+
+	public float minIncDeg = 320.0f;
+	public float maxIncDeg = 359.9f;
+	// Use this for initialization
+	void Start () {
+		self = GetComponent<Transform>();
+		sun  = self.Find("Sun").GetComponent<Light>();
+
+		if (sun != null) {
+			sunIntensity = sun.intensity;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rotationSpeed = orbitSpeed * Time.deltaTime;
+		transform.Rotate(0,0,rotationSpeed);
+	}
+
+	void FixedUpdate() {
+		if (self.rotation.eulerAngles.z > minDimDeg && self.rotation.eulerAngles.z < maxDimDeg) {
+			sun.intensity = sunIntensity * (maxDimDeg - self.rotation.eulerAngles.z) / (maxDimDeg - minDimDeg);
+		} else if (self.rotation.eulerAngles.z > minIncDeg && self.rotation.eulerAngles.z < maxIncDeg) {
+			sun.intensity = sunIntensity * (self.rotation.eulerAngles.z - minIncDeg) / (maxIncDeg - minIncDeg);
+		}
+	}
+}
