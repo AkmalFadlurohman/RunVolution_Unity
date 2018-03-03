0bec0abaea04d33d46bfc44d60dc6888f1938f42
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-03-03 21:26:59 +0800

    Added camera movement

---
+++ Assets/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CameraMovement : MonoBehaviour, IPointerDownHandler {
+
+	public float rotateSpeed;
+	public int dir;
+	public GameObject target;
+
+	void Awake() {
+		if (dir < 0) {
+			dir = -1;
+		} else if (dir > 0) {
+			dir = 1;
+		}
+	}
+	
+	void rotateY() {
+		var y = rotateSpeed * dir;
+		target.transform.Rotate(0,y,0);
+	}
+
+	public void OnPointerDown(PointerEventData eventData) {
+		if (Input.GetMouseButton(0)) {
+			rotateY();
+		}
+	}
+}
