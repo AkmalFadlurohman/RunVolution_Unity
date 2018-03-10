0ad9b04fc9f5cbe280e94437065d57274e31627f
ama <ama@amas-iMac.local>
2018-03-10 21:20:12 +0700

    Change UI and add login button

---
+++ Assets/Assets/Scripts/Login.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+
+	public InputField emailField;
+	public InputField passwordField;
+	public GameObject gameObject;
+	public GameObject targetObject;
+
+	public void login() {
+		
+		Debug.Log ("Clicked login");
+		string email = emailField.text;
+		string password = passwordField.text;
+		Debug.Log ("User with email " + email + " has logged");
+		gameObject.SetActive(false);
+		targetObject.SetActive (true);
+	}
+}
