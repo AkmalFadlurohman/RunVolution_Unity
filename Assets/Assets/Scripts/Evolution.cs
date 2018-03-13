92d2fa20a8346cac20f3bf6d11b01b93650c87d9
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-03-13 17:56:53 +0700

    Created evolution rotation.

--- Assets/Assets/Scripts/Evolution.cs
+++ Assets/Assets/Scripts/Evolution.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Evolution : MonoBehaviour {
-	public GameObject self;
+	public GameObject monster;
+	public GameObject camera;
 	public float rotation = 0;
 	public float acc = 5;
-	public float maxSpeed = 300.0f;
+	public float cameraSpeed = 0f;
+	public float cameraAcc = 0.001f;
+	public float maxSpeed = 100.0f;
+	private bool accelerating = true;
 
 	void Start() {
 		
 	}
 
 	void Update() {
-		if (rotation < maxSpeed); {
-			rotation = (30 + acc) * Time.deltaTime;
-			self.transform.Rotate(0, rotation, 0);
-			acc += acc;
+		if (accelerating) {
+			rotation += acc;
+			monster.transform.Rotate(0, rotation, 0);	
+
+			cameraSpeed += cameraAcc;
+			camera.transform.Translate(0, 0, cameraSpeed);
+
+			if (rotation >= maxSpeed) {
+				accelerating = false;
+			}
+		} else {
+			rotation -= acc;
+			monster.transform.Rotate(0, rotation, 0);
+
+			cameraSpeed -= cameraAcc;
+			camera.transform.Translate(0, 0, -cameraSpeed);
+
+			if (rotation <= 0) {
+				accelerating = true;
+			}
 		}
 	}
 }
